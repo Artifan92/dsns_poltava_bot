54752130d385d5276040c953a50317e0c4ca7b0d
Artem Balandin <artem_balandin@outlook.com>
2022-09-08 17:18:00 +0300

    some changes

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-import bot from './modules/botInit.js';
+import TelegramApi from 'node-telegram-bot-api';
+import { token } from './services/secret.js';
 import { commands, keyboard, callback } from './services/data.js';
 import Commands from './modules/Commands.js';
 import Keyboard from './modules/Keyboard.js';
 import Callback from './modules/Callback.js';
+
+/** BOT INIT */
+const bot = new TelegramApi(token, { polling: true });
 
 /** SET COMMANDS BOT */
 const setCommandsBot = new Commands(commands, bot);
--- modules/botInit.js
+++
@@ -1,6 +0,0 @@
-import TelegramApi from 'node-telegram-bot-api';
-import { token } from '../services/secret.js';
-
-const bot = new TelegramApi(token, { polling: true });
-
-export default bot;
