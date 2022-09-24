432a96ba6bdf57fa3c3b90541982cdbc28d793c3
Artem Balandin <artem_balandin@outlook.com>
2022-09-24 11:16:21 +0300

    Add Alarm Message to User Schema

--- data/dbModels.js
+++ data/dbModels.js
@@ -32,6 +32,10 @@ const UserSchema = new mongoose.Schema({
 			type: Boolean,
 			default: false,
 		},
+		alarm_message: {
+			type: Boolean,
+			default: true,
+		},
 	}),
 	CommandSchema = new mongoose.Schema({
 		command: String,
